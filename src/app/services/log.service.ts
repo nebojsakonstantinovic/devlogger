79f313af7be502d2fa5a9f5951552e514ba27578
Nebojsa Konstantinovic <nebojsa.konstantin@gmail.com>
2018-05-19 22:53:48 +0200

    devlogger2

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { Observable, of } from 'rxjs';
 
 import { Log } from '../models/Log'; 
 
@@ -9,16 +12,52 @@ export class LogService {
   // Properties
   logs: Log[];
 
+  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+  selectedLog = this.logSource.asObservable();
+
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
 
   constructor() {
-    this.logs = [
-      {id: '1', text: 'Generated components', date: new Date('2/26/2018 12:35:12')},
-      {id: '2', text: 'Added Bootstrap', date: new Date('2/27/2018 9:39:53')},
-      {id: '3', text: 'Added logs components', date: new Date('2/28/2018 14:15:44')}
-    ]
+    // this.logs = [
+    //   {id: '1', text: 'Generated components', date: new Date('2/26/2018 12:35:12')},
+    //   {id: '2', text: 'Added Bootstrap', date: new Date('2/27/2018 9:39:53')},
+    //   {id: '3', text: 'Added logs components', date: new Date('2/28/2018 14:15:44')}
+    // ]
+    this.logs = [];
   }
 
-  getLogs() {
-    return this.logs;
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+    
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+  }
+
+  clearState() {
+    this.stateSource.next(true);
   }
 }
