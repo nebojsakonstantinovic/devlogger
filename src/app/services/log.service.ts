4cd8e41f8c755d7b1b6501d8efd26f48af165f62
Nebojsa Konstantinovic <nebojsa.konstantin@gmail.com>
2018-05-19 23:08:15 +0200

    LS added

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -28,7 +28,15 @@ export class LogService {
   }
 
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    if (localStorage.getItem('logs') === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+
+    return of(this.logs.sort((a, b) => {
+      return b.date -a.date;
+    }));
   }
 
   setFormLog(log: Log) {
@@ -37,6 +45,9 @@ export class LogService {
 
   addLog(log: Log) {
     this.logs.unshift(log);
+
+    // Add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   updateLog(log: Log) {
@@ -47,6 +58,8 @@ export class LogService {
     });
     this.logs.unshift(log);
     
+    // Update local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   deleteLog(log: Log) {
@@ -55,6 +68,9 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+
+    // Delete form local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   clearState() {
